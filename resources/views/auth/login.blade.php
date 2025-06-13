0b3eefd2e94d2c8c364619281ecce9fa058913e1
DiogoCesar <diogocesar.2127@gmail.com>
2025-06-13 18:57:59 -0400

    feat: ajustes no front-end (área de trabalho, login, registro e produtos)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,88 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Login</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Login Empório</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="../../vendors/feather/feather.css">
+  <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="../../vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../../css/vertical-layout-light/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="../../images/favicon.png" />
 </head>
+
 <body>
-    <h2>Login</h2>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        <div class="row w-100 mx-0">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+              <div class="brand-logo">
+                <img src="../../images/logo.svg" alt="logo">
+              </div>
+              <h4>Olá, Vamos Começar!</h4>
+              <h6 class="font-weight-light">Insira suas credenciais para entrar</h6>
+              
+              @if(session('erro'))
+                <p style="color: red">{{ session('erro') }}</p>
+              @endif
 
-    @if(session('erro'))
-        <p style="color: red">{{ session('erro') }}</p>
-    @endif
+              @if(session('sucesso'))
+                <p style="color: green">{{ session('sucesso') }}</p>
+              @endif
 
-    @if(session('sucesso'))
-        <p style="color: green">{{ session('sucesso') }}</p>
-    @endif
-
-    <form method="POST" action="{{ route('login.usuario') }}">
-        @csrf
-        <label for="login">Email:</label>
-        <input type="email" id="login" name="login" required><br><br>
-
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha" required><br><br>
-
-        <button type="submit">Entrar</button>
-    </form>
-
-    <p>Ainda não tem conta? <a href="{{ route('registro.form') }}">Registrar</a></p>
+              <form class="pt-3" method="POST" action="{{ route('login.usuario') }}">
+                @csrf
+                <div class="form-group">
+                  <input type="email" class="form-control form-control-lg" id="login" name="login" required placeholder="Email">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control form-control-lg" name="senha" required id="senha" placeholder="Digite sua senha">
+                </div>
+                <div class="mt-3">
+                  <button type="submit" class="btn btn-block btn-info btn-lg font-weight-medium auth-form-btn"><b>ENTRAR</b></button>
+                </div>
+                <div class="my-2 d-flex justify-content-between align-items-center">
+                </div>
+                <div class="mb-2">
+                  <button type="button" class="btn btn-warning btn-icon-text ">
+                    <i class="ti-alert btn-icon-prepend"></i>Esqueceu a senha? Entre em contato com o TI.
+                  </button>
+                </div>
+                <div class="text-center mt-4 font-weight-light">
+                  Ainda não possui uma conta? <a href="{{ route('registro.form') }}" class="text-primary">Criar Login</a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="../../vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="../../js/off-canvas.js"></script>
+  <script src="../../js/hoverable-collapse.js"></script>
+  <script src="../../js/template.js"></script>
+  <script src="../../js/settings.js"></script>
+  <script src="../../js/todolist.js"></script>
+  <!-- endinject -->
 </body>
+
 </html>
--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -28,17 +28,113 @@
       </div>
     </nav>
     <!-- partial -->
+      <!-- INÍCIO - BARRA SUPERIOR -->
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+        <a class="navbar-brand brand-logo mr-5" href="index.html"><img src="images/logo-h.png" class="mr-2" alt="logo"/></a>
+        <a class="navbar-brand brand-logo-mini" href="index.html"><img src="images/logo-mini.svg" width="15rem" alt="logo"/></a>
+      </div>
+      <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+          <span class="icon-menu"></span>
+        </button>
+
+        <!-- Foto do Usuário -->
+        <ul class="navbar-nav navbar-nav-right">
+          <li class="nav-item nav-profile dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
+              <img src="images/faces/face28.jpg" alt="profile"/>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+              @if(session('usuario'))
+              <form method="POST" action="{{ route('logout') }}"></form>
+              @csrf
+              <button type="submit" class="dropdown-item">
+                <i class="ti-power-off text-primary"></i>
+                Logout/Sair
+              </button>
+              </form>
+              @endif
+            </div>
+          </li>
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="icon-menu"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- FIM - BARRA SUPERIOR -->
+
+
+    <!-- partial -->
+     <!-- INÍCIO - BARRA LATERAL -->
     <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_settings-panel.html -->
+      <!-- partial -->
+      <!-- partial:partials/_sidebar.html -->
       <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <ul class="nav">
           <li class="nav-item">
-            <a class="nav-link" href="../../index.html">
+            <a class="nav-link" href="index.html">
               <i class="icon-grid menu-icon"></i>
+              <span class="menu-title">Página Inicial</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+              <i class="icon-head menu-icon"></i>
+              <span class="menu-title">Usuário</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="auth">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="{{ route('registro.form') }}"> Criar Usuário </a></li>
+              @if(session('usuario'))
+              <form method="POST" action="{{ route('logout') }}"></form>
+              @csrf
+              <li class="nav-item">
+              <button type="submit" class="nav-link">
+                Logout/Sair
+              </button>
+              </li>
+              </form>
+              @endif
+              </ul>
+            </div>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="pages/documentation/documentation.html">
+              <i class="icon-paper menu-icon"></i>
+              <span class="menu-title">Vendas</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('produtos.index') }}">
+              <i class="icon-paper menu-icon"></i>
               <span class="menu-title">Produtos</span>
             </a>
           </li>
+
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#error" aria-expanded="false" aria-controls="error">
+              <i class="icon-ban menu-icon"></i>
+              <span class="menu-title">Páginas de Erro</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="error">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
+              </ul>
+            </div>
+          </li>
+
         </ul>
       </nav>
+      <!-- FIM - BARRA LATERAL -->
       <!-- partial -->
       <div class="main-panel">
         <div class="content-wrapper">
@@ -96,7 +192,6 @@
           </div>
         </footer>
       </div>
-    </div>
   </div>
 
 </body>
