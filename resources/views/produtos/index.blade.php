420a981c54faf6b156ea0778c0c0705e6f853ce3
goemon009 <leucampos@gmail.com>
2025-06-20 19:52:29 -0400

    Corrigindo a view de produtos que eu alterei por engano

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -1,43 +1,205 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-BR">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Clientes - Teste</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Emporio Seu Prozo</title>
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" />
 </head>
+
 <body>
-    <h1>Lista de Clientes (Teste)</h1>
+  <div class="container-scroller">
+    <!-- partial:../../partials/_navbar.html -->
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+        <a class="navbar-brand brand-logo mr-5" href="index.html"><img src="{{ asset('images/logo-h.png') }}" class="mr-2" alt="logo"/></a>
+        <a class="navbar-brand brand-logo-mini" href="index.html"><img src="{{ asset('images/logo-mini.svg') }}" width="15rem" alt="logo"/></a>
+      </div>
+      <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+          <span class="icon-menu"></span>
+        </button>
+  
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="icon-menu"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- partial -->
+      <!-- INÍCIO - BARRA SUPERIOR -->
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+        <a class="navbar-brand brand-logo mr-5" href="index.html"><img src="images/logo-h.png" class="mr-2" alt="logo"/></a>
+        <a class="navbar-brand brand-logo-mini" href="index.html"><img src="images/logo-mini.svg" width="15rem" alt="logo"/></a>
+      </div>
+      <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+          <span class="icon-menu"></span>
+        </button>
 
-    <a href="{{ route('clientes.create') }}">Novo Cliente</a>
+        <!-- Foto do Usuário -->
+        <ul class="navbar-nav navbar-nav-right">
+          <li class="nav-item nav-profile dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
+              <img src="images/faces/face28.jpg" alt="profile"/>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+              @if(session('usuario'))
+              <form method="POST" action="{{ route('logout') }}"></form>
+              @csrf
+              <button type="submit" class="dropdown-item">
+                <i class="ti-power-off text-primary"></i>
+                Logout/Sair
+              </button>
+              </form>
+              @endif
+            </div>
+          </li>
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="icon-menu"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- FIM - BARRA SUPERIOR -->
 
-    <table border="1" cellpadding="8" cellspacing="0">
-      
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Email</th>
-                <th>Telefone</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($clientes as $cliente)
-                <tr>
-                    <td>{{ $cliente->id_cliente }}</td>
-                    <td>{{ $cliente->nome }}</td>
-                    <td>{{ $cliente->email }}</td>
-                    <td>{{ $cliente->telefone }}</td>
-                    <td>
-                        <a href="{{ route('clientes.edit', $cliente->id_cliente) }}">Editar</a> |
-                        <form action="{{ route('clientes.destroy', $cliente->id_cliente) }}" method="POST" style="display:inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">Excluir</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+
+    <!-- partial -->
+     <!-- INÍCIO - BARRA LATERAL -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_settings-panel.html -->
+      <!-- partial -->
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">
+              <i class="icon-grid menu-icon"></i>
+              <span class="menu-title">Página Inicial</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+              <i class="icon-head menu-icon"></i>
+              <span class="menu-title">Usuário</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="auth">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="{{ route('registro.form') }}"> Criar Usuário </a></li>
+              @if(session('usuario'))
+              <form method="POST" action="{{ route('logout') }}"></form>
+              @csrf
+              <li class="nav-item">
+              <button type="submit" class="nav-link">
+                Logout/Sair
+              </button>
+              </li>
+              </form>
+              @endif
+              </ul>
+            </div>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="pages/documentation/documentation.html">
+              <i class="icon-paper menu-icon"></i>
+              <span class="menu-title">Vendas</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('produtos.index') }}">
+              <i class="icon-paper menu-icon"></i>
+              <span class="menu-title">Produtos</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#error" aria-expanded="false" aria-controls="error">
+              <i class="icon-ban menu-icon"></i>
+              <span class="menu-title">Páginas de Erro</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="error">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
+              </ul>
+            </div>
+          </li>
+
+        </ul>
+      </nav>
+      <!-- FIM - BARRA LATERAL -->
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="row">
+            <div class="col-lg-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between align-items-center mb-3">
+                    <h4 class="card-title m-0">Meus Produtos</h4>
+                    <a href="{{ route('produtos.create') }}" class="btn btn-info">
+                      Adicionar
+                    </a>
+                  </div>
+                  <div class="table-responsive">
+                    <table class="table table-hover">
+                      <thead>
+                        <tr>
+                          <th>Código</th>
+                          <th>Nome</th>
+                          <th>Preço</th>
+                          <th>Quantidade de Estoque </th>
+                          <th>Ações</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach ($produtos as $produto)
+                            <tr>
+                            <td>{{$produto->id_produto}}</td>
+                            <td>{{$produto->nome}}</td>
+                            <td>{{$produto->preco}}</td>
+                            <td>{{$produto->quantidade_estoque}}</td>
+                            <td>
+                              <a class="btn btn-info" href="{{ route('produtos.edit', $produto) }}">
+                                Editar
+                              </a>
+                              <form action="{{ route('produtos.destroy', $produto) }}" method="POST" style="display:inline;" onsubmit="return confirmarExclusao(this)">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Excluir</button>
+                            </form>
+                            </td>
+                          </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2025.<a href="https://www.bootstrapdash.com/" target="_blank">Grupo Bravos TI</a></span>
+          </div>
+        </footer>
+      </div>
+  </div>
+
 </body>
+
 </html>
+
+<script>
+  function confirmarExclusao(form) {
+    return confirm('Tem certeza que deseja excluir este produto?');
+  }
+</script>
